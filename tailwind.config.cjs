509c70bf4926dd47e1798d74e5b5018f6e1f4061
William Hideki Nakata <whnakata@gmail.com>
2023-04-19 12:56:45 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      },
+      colors: {
+        "fuel-yellow": {
+          "50": "#fdfae8",
+          "100": "#faf3c6",
+          "200": "#f6e593",
+          "300": "#eecd59",
+          "400": "#e3b426",
+          "500": "#cf9b20",
+          "600": "#b07a1c",
+          "700": "#8d581b",
+          "800": "#734921",
+          "900": "#613d23",
+          "950": "#371e10"
+        },
+        "bermuda": {
+          "50": "#f1fcf8",
+          "100": "#d0f7ea",
+          "200": "#a1eed5",
+          "300": "#77e0c2",
+          "400": "#3cc5a2",
+          "500": "#23a989",
+          "600": "#198870",
+          "700": "#186d5b",
+          "800": "#18574b",
+          "900": "#18493f",
+          "950": "#082b26"
+        }
+
+
+      }
+    }
+  },
+  plugins: []
+};
+
