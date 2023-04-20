8453a9b4553309c701a2202c6feb6ef96b8e724a
William Hideki Nakata <whnakata@gmail.com>
2023-04-20 10:05:33 -0300

    create routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React, { useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import ScrollContainer from "./components/ScrollContainer";
+import poster from "./assets/poster.png";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Default from "./layouts/Default";
+import Page404 from "./pages/Page404";
 
 function App() {
-  const maincontentRef = useRef(null);
-
   return (
-    <div className="App min-h-screen ">
-      <Navbar />
-      <div className={"flex h-[200vh] flex-col justify-between"}>
-        <span>a</span>
-        <span>a</span>
-        <span>a</span>
-        <span>a</span>
-      </div>
+    <div className="App ">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Default />}>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+          <Route path="contact" element={<h1>Contato</h1>} />
+          <Route path={"*"} element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
