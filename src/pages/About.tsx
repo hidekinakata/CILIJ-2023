2fbcc57837b3c6081c5af5c5d19e97b71e266530
William Hideki Nakata <whnakata@gmail.com>
2023-04-20 10:05:07 -0300

    Create About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type AboutType = {};
+
+const About: React.FC<AboutType> = (props) => {
+  return (
+    <div>
+      <h1>Informações</h1>
+    </div>
+  );
+};
+
+export default About;
