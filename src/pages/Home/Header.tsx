a24daef0a369493264fcecc6e1baeb36309f1d7e
William Hideki Nakata <whnakata@gmail.com>
2023-04-24 16:13:49 -0300

    make header responsive and add poster

--- src/pages/Home/Header.tsx
+++ src/pages/Home/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import poster from "../../assets/poster.png";
+import SubscribeBtn from "../../components/SubscribeBtn";
 
 type HomeType = {};
 
@@ -7,36 +8,53 @@ const Header: React.FC<HomeType> = (props) => {
   return (
     <section
       className={
-        "relative flex min-h-screen w-full items-center px-12 pt-20 lg:px-32"
+        "relative flex min-h-screen w-full items-center justify-center px-12 pt-20 lg:px-32"
       }
-      style={{
-        background: `linear-gradient(90deg, rgba(255,255,255,1) 44%, rgba(255,255,255,0) 100%), url(${poster})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "auto 180vh",
-        backgroundPosition: "right",
-      }}
+      // style={{
+      //   background: `linear-gradient(90deg, rgba(255,255,255,1) 44%, rgba(255,255,255,0) 100%), url(${poster})`,
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundSize: "auto 180vh",
+      //   backgroundPosition: "right",
+      // }}
     >
-      <div className={"flex flex-col"}>
-        <h3 className={"text-lg"}>
-          <span className={"text-red-800"}>VII</span> Congresso Internacional de
-          <br />
-          <span className={"text-red-800"}>Literatura Infantil e Juvenil</span>
-        </h3>
-        <h1 className={"text-4xl font-black md:max-w-screen-sm md:text-6xl"}>
-          Educação literária:
-          <span className={"font-['Caveat'] text-red-700"}>
-            {" "}
-            Mudanças em movimento
-          </span>
-        </h1>
-        <div className={"mt-10 w-fit"}>
-          <a
-            className={
-              "cursor-pointer rounded-full bg-bermuda-500 p-2 px-4 text-white hover:bg-bermuda-400"
-            }
-          >
-            Faça sua inscrição
-          </a>
+      <div
+        className={
+          "flex h-fit flex-col-reverse items-center justify-center md:flex-row md:justify-start"
+        }
+      >
+        <div
+          className={
+            "mt-6 flex h-full flex-col justify-center text-center md:mt-0 md:justify-start md:text-left"
+          }
+        >
+          <h3 className={"text-lg"}>
+            <span className={"text-red-800"}>VII</span> Congresso Internacional
+            de
+            <br />
+            <span className={"text-red-800"}>
+              Literatura Infantil e Juvenil
+            </span>
+          </h3>
+          <h1 className={"text-3xl font-black md:max-w-screen-sm md:text-6xl "}>
+            Educação literária:
+            <span className={"font-['Caveat'] text-red-700"}>
+              <span className={"md:hidden"}>
+                <br />
+              </span>
+              Mudanças em movimento
+            </span>
+          </h1>
+          <div className={"mt-10 w-fit self-center"}>
+            <SubscribeBtn />
+          </div>
+        </div>
+
+        <div className={"max-h-[40vh] max-w-full md:max-h-[55vh]"}>
+          <img
+            className={"max-h-[40vh] max-w-full md:max-h-[55vh]"}
+            src={poster}
+            alt={"Event Poster"}
+          />
         </div>
       </div>
     </section>
