8fb33bad3f4b5f35d7e1e9b76880372aac6a7532
William Hideki Nakata <whnakata@gmail.com>
2023-04-24 17:30:27 -0300

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { BiCopyright } from "react-icons/bi";
 
 type FooterType = {};
 
 const Footer: React.FC<FooterType> = (props) => {
-  return <></>;
+  return (
+    <footer
+      className={
+        "flex h-60 w-full items-center bg-aubergine-950 font-medium text-white"
+      }
+    >
+      <div className={"w-full whitespace-pre-wrap"}>
+        <p className={"flex w-full items-center justify-center gap-1"}>
+          Copyright
+          <span className={"whitespace-nowrap"}>
+            <BiCopyright />
+          </span>
+          2023
+        </p>
+        <p className={" w-full text-center"}>PROLELI</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 type DefaultType = React.PropsWithChildren & {};
 
@@ -9,6 +10,7 @@ const Default: React.FC<DefaultType> = ({ children }) => {
     <div className={"min-h-screen"}>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
