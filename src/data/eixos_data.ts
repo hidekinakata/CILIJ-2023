0cd1e611a65f1914031d6c84c2fe33ca21ff2459
William Hideki Nakata <whnakata@gmail.com>
2023-04-27 17:52:00 -0300

    Create eixos_data.ts

---
+++ src/data/eixos_data.ts
@@ -0,0 +1,153 @@
+type Eixo = {
+  eixo: number;
+  title: string;
+  presenters: string[];
+  email: string;
+  resume: string;
+};
+
+const Eixos: Eixo[] = [
+  {
+    eixo: 1,
+    title: "Literatura infantil para crianças menores de seis anos",
+    resume:
+      "Desde o nascimento, vamos inscrevendo a criança no universo simbólico. Gestos e balbucios, inicialmente desprovidos de significados, vão, pouco a pouco, se constituindo em atividades simbólicas, a partir das interações que se estabelecem entre bebês e sujeitos mais experientes. As cantigas de ninar, parlendas, jogos de rimas são atividades que promovem essa construção de sentidos. Essa literatura de tradição oral representa uma das primeiras linguagens literárias com a qual as crianças entram em contato (Com o quê?). A medida que ampliam suas experiências com a cultura escrita, bebês e demais crianças pequenas vão podendo cada vez mais se apropriar da linguagem própria dos livros e do próprio livro como produto cultural. Considerando os bebês e as demais crianças menores de seis anos de idade, algumas questões, entre o outras, poderiam ser suscitadas: Como a literatura oral vem sendo trabalhada nas instituições de educação infantil? Que livros são adequados para crianças que ainda não leem e não escrevem convencionalmente? Quais práticas de leitura seriam adequadas a cada uma das faixas etárias que compõem a primeira infância? Como a educação infantil se relaciona com a literatura? Como deveria se relacionar? Como o mercado editorial tem se comportado em relação à produção para essa faixa etária? Como se caracteriza a literatura brasileira para bebês e crianças menores de seis anos? Que tendências podemos perceber, doravante, no processo de sua constituição?  As bibliotecas e outros espaços culturais têm se dedicado aos pequenos leitores? Quais projetos ou atividades vêm sendo desenvolvidos nesses espaços com o intuito de apoiar a formação do pequeno leitor? Vêm sendo implantadas políticas públicas que favorecem o acesso dessas crianças ao universo literário? Como elas se caracterizam? Tais questões procuram nortear e sustentar as propostas de comunicação científica de pesquisas que se debruçam sobre a problemática esboçada para o aqui delineado, Eixo ‘Literatura infantil para crianças muito pequenas’, cujas ações advogam a favor de uma Educação Literária desde a Primeira Infância.",
+    presenters: [
+      "Cyntia Graziella Guizelim Simões Girotto (UNESP)",
+      "Mônica Correia Baptista (UFMG)",
+      "Sandra Franco (UEL)",
+      "Cassia Bittens (Literatura de Berço)",
+      "Rita Lopes (UFOP)",
+    ],
+    email: "eixo1cilij@gmail.com",
+  },
+  {
+    eixo: 2,
+    title: "Poesia e oralidade",
+    resume:
+      "A poesia construída a partir da palavra tem a possibilidade de abrir espaços para outros elementos que ajudam a cativar o leitor e estimulá-lo a expandir a leitura e a sensibilidade do seu entorno, especialmente no contexto escolar. Rimas, musicalidade, ilogismo, imagens e enigmas são alguns dos recursos que levam o texto poético a mobilizar a atenção e os sentimentos de quem está lendo ou ouvindo, ainda mais se esse público for constituído de crianças. No ambiente de escolarização, entretanto, nem sempre a arte poética encontra os espaços necessários para ganhar a atenção que deveria. Assim, o objetivo deste Eixo é proporcionar a discussão a respeito da natureza da poesia e das potencialidades de oralização desse texto, a fim de integrar-se as práticas cotidianas de aprendizagem, em ambientes escolarizados ou não, sem perder de vista os possíveis conhecimentos que o gênero pode proporcionar.",
+    presenters: [
+      "José Hélder Pinheiro Alves (UFCG)",
+      "Rosemary Lapa de Oliveira (UEB)",
+      "Amanda Viliego (UFSJ)",
+      "Ivanete Bernardino Soares (UFOP)",
+    ],
+    email: "eixo2cilij@gmail.com",
+  },
+  {
+    eixo: 3,
+    title: "Literatura infantil e juvenil e temas polêmicos",
+    resume:
+      '“Temas polêmicos”, "temas fraturantes", “temas delicados”, “temas tabu” são algumas das formas de nomear temas como: a morte, a violência, o medo, o abandono, a separação dos pais, a deficiência, a doença, a drogadição, o aborto, a sexualidade, o racismo, o preconceito, as guerras, entre outros,  quando presentes em livros literários direcionados à infância e à adolescência. Tal nomeação aponta para o desafio que tal abordagem acarreta, em função do risco do didatismo nos livros ou – em termos mais amplos – da dificuldade de aceitação de tais obras nos circuitos de leitura (quais circuitos?). Nesse sentido, o  Eixo temático pretende se abrir a discussões que problematizem os temas polêmicos, aceitando  tanto ensaios de cunho mais teórico sobre a articulação entre qualquer desses temas e a literatura infantil e juvenil, quanto análises de obras específicas (contemporâneas ou de outros momentos históricos) que os explorem – de maneira central ou secundária, por meio das personagens, por exemplo -  e, ainda, trabalhos que incorporem a análise da leitura de obra(s) que contemple(m) tais temáticas, por crianças e/ou adolescentes.',
+    presenters: [
+      "Literatura infantil e juvenil e temas polêmicos",
+      "Rosa Maria Hessel Silveira (UFRGS)",
+      "Edgar Roberto Kirchof (ULBRA)",
+      "Pedro Afonso Barth (UFU)",
+      "Rodrigo Corrêa Martins Silva Machado (UFOP)",
+    ],
+    email: "eixo3cilij@gmail.com",
+  },
+  {
+    eixo: 4,
+    title: "A literatura juvenil e jovens leitores",
+    resume: `As pesquisas realizadas nos últimos anos, tomando por objeto a literatura juvenil, atestam sua pertinência no meio acadêmico e educacional. Paralelo ao vasto número de publicações do mercado editorial e aos prêmios concedidos a escritores e ilustradores de destaque, a maturidade como subsistema literário torna-se aspecto inegável de sua circulação. Desse modo, refletir sobre a literatura juvenil é o objetivo deste eixo, que abarca estudos acerca de: sua relevância para o campo de estudos literários no contexto brasileiro contemporâneo; sua presença e suas tendências no sistema literário brasileiro; sua composição estética e suas materialidades como composição estética; seus diversos suportes e suas inter-relações com o texto literário; e também a escolarização da literatura e a leitura literária no quadro proposto pela BNCC. As propostas de análises críticas sobre os mais diversos textos literários identificados como pertencentes a este corpus também constituem material a ser apresentado e discutido entre os participantes. Finalmente, cabem também estudos comparativos, críticos e históricos, de modo geral, que contribuam para as discussões.`,
+    presenters: [
+      "Thiago Alves Valente (UENP)",
+      "Elizabeth da Penha Cardoso (PUC-SP)",
+      "Rosana Rodrigues (UEMT)",
+      "Juliana Pádua (UNIFESSPA)",
+    ],
+    email: "eixo4cilij@gmail.com",
+  },
+  {
+    eixo: 5,
+    title: "Literatura infantil e juvenil e as múltiplas linguagens",
+    resume: `Considerando o papel que a literatura teve ao longo dos tempos de representar a experiência humana em sua diversidade, cabe refletir sobre as múltiplas linguagens por meio das quais a literatura infantil e juvenil contemporânea se apresenta, bem como o papel que os novos suportes e artifícios adquirem na sedução e envolvimento dos leitores. Dessa forma, o presente eixo pretende ser um espaço para o compartilhamento de pesquisas e experiências que enfoquem a escolarização deste acervo bem como a crítica que a ele se volta. Ficou pequeno se comparado aos demais.`,
+    presenters: [
+      "Fabiane Verardi Burlamaque (UPF)",
+      "Diogenes Buenos Aires de Carvalho (UEPI)",
+      "Regina Michelli (UERJ)",
+      "Adriana Lins Precioso (UEMT)",
+    ],
+    email: "eixo5cilij@gmail.com",
+  },
+  {
+    eixo: 6,
+    title: "Literatura infantil e ensino",
+    resume: `Tendo seu surgimento condicionado às transformações sócio históricas que deram origem à concepção de infância, a literatura infantil se desenvolveu e se consolidou como arte comprometida aos interesses da família e da escola. Desse modo, atende às exigências do contexto em que está inserida, o que fica claro no projeto de nacionalização que marca a literatura infantil na virada do século XIX. Os caminhos que essa literatura percorre que vão do idealismo exaltado e didatismo ao lúdico e à interação com o leitor, desaguam na contemporaneidade em espaço multicultural e transdisciplinar em que a voz de narradores e personagens discutem diferentes conflitos identitários da criança. A literatura infantil contemporânea se reconfigura no diálogo com a tradição oral e popular, com as vanguardas artísticas, os posicionamentos políticos, as tecnologias e abre novas possibilidades de leitura e interação com o leitor e o mediador da leitura no livro pensando como objeto artístico e no espaço da web. Atendendo à formação do leitor, nesse novo contexto, as propostas ou estratégias no ensino da leitura literária visam o envolvimento da criança no processo de construção de sentidos. Nesse percurso, os gêneros que compõem a literatura infantil são considerados em suas especificidades e diferentes formas de adaptação e no diálogo com a tradição. Os contos de fadas, as narrativas dramatizadas, os poemas que convidam ao lúdico, são algumas das formas apresentadas que, nutridas do imaginário infantil, convidam o leitor à participação de um novo mundo que se apresenta, que nasce no processo da leitura. Para este Eixo, serão aceitos trabalhos que abarquem História, Crítica e Interpretação da produção literária infantil nacional e internacional. Tradução, adaptação, mídias digitais, e-books e apps e processos artísticos. Estudos de obras da literatura infantil e relatos de práticas de trabalhos que vão desde a leitura interpretativa da ilustração, às propostas com diferentes gêneros (poesia, teatro, conto e romance, HQs, literatura em quadrinhos, mitos e lendas, etc.).`,
+    presenters: [
+      "Daniela Segabinazi (UFPB)",
+      "Elisa Maria Dalla-Bona (UFPR)",
+      "Maria Laura Pozzobon Spengler (Instituto Federal Catarinense)",
+      "Diana Maria Lopes Saldanha (UERN)",
+    ],
+    email: "eixo6cilij@gmail.com",
+  },
+  {
+    eixo: 7,
+    title: "A leitura literária: espaços de mediação e de formação de leitores",
+    resume: `Este Eixo caracteriza-se por refletir sobre os mecanismos e as práticas que se realizam, em espaços formais ou não formais, de modo a oportunizar aos leitores, especialmente aqueles que se encontram no início de sua formação literária, o acesso à literatura infantojuvenil, entendida como um bem cultural que deve ser apropriado pelas pessoas durante sua formação escolar ou social. Vincula-se a investigações acerca do espaço pedagógico que a literatura tem, em especial, nas práticas cotidianas escolares de incentivo à formação de leitores, bem como às atividades e ou estratégias que são utilizadas com esse objetivo, tais como: a contação de histórias; a disseminação da literatura em sala de aula; o uso da biblioteca; a literatura infantil e juvenil e suas manifestações ao longo dos tempos; as relações entre literatura e o suporte digital, entre outros aspectos. Com isso, evidencia-se o fazer pedagógico, de modo que se discutam as relações e os saberes em prol da formação de leitores de literatura.    `,
+    presenters: [
+      "Alcione Maria dos Santos (UFMS)",
+      "Leoneide Martins (UFMA)",
+      "Rovilson José da Silva (UEL)",
+      "Leonardo Montes Lopes (UniRV)",
+    ],
+    email: "eixo7cilij@gmail.com",
+  },
+  {
+    eixo: 8,
+    title: "A leitura literária: espaços de mediação e de formação de leitores",
+    resume:
+      "O Eixo congrega pesquisas, trabalhos de extensão e ensino que focalizam aspectos teóricos/metodológicos relacionados a práticas pedagógicas e ao ensino de literaturas em Língua Portuguesa (LsELP), à mediação de leitura e  a formação de leitores em um contexto que contemple a  leitura como ato formativo para o leitor em construção. Entendemos, como premissa para os estudos do Eixo, a leitura literária como de fundamental importância na formação do leitor em diferentes espaços formativos. Pensamos, ainda, nos trabalhos do Eixo incluir reflexões sobre a formação do docente de Letras, Pedagogia e áreas afins como provocação a uma formação continuada ligada  à leitura literária em um sentido amplo que congrega, portanto, a diversidade de enfoques da leitura não só do texto como do mundo. A proposta tem como principal foco, por meio dos diferentes trabalhos apresentados no Eixo, refletir sobre as práticas pedagógicas e a  situação do ensino de literaturas em Língua Portuguesa na Educação Básica e, na medida do possível, traçar um perfil do professor de literatura em âmbito da formação acadêmica e políticas públicas ligadas ao ensino e difusão da leitura literária que pensem a formação de leitores e a mediação de leitura em um sentido amplo, no qual residem as concepções de letramento literário e educação literária.",
+    presenters: [
+      "Eliane Santana Dias Debus (UFSC)",
+      "Silvana Augusta Barbosa Carrijo (UFJataí)",
+      "Danglei de Castro Pereira (UNB)",
+      "Patricia Beraldo Romano (UNIFESSPA)",
+    ],
+    email: "eixo8cilij@gmail.com",
+  },
+  {
+    eixo: 9,
+    title: "Literatura e estratégias de leitura",
+    resume:
+      "O principal foco deste eixo é o de pensar práticas possíveis para a formação do leitor literário que partem do entrelaçamento da literatura com as estratégias de leitura. Nesse vasto campo de estudos, consideramos importante o papel da literatura na formação do leitor e sendo esse um dos objetivos da escola é preciso buscar meios para que isso aconteça. As estratégias de leitura possibilitam a compreensão do texto literário e como fio condutor dos debates podemos refletir: Como desenvolver as estratégias de leitura via literatura no ambiente escolar? Como promover a mediação do texto sem negar espaço à subjetividade dos leitores nem negar a importância da ocupação, pelo professor, de um lugar de leitor especializado? Como tornar a leitura do texto literário uma experiência estética, que dê conta da especificidade artística da literatura? Esperamos que diversas pesquisas possam contribuir para o debate, visto que a formação do leitor literário é um processo cujo espaço privilegiado para acontecer ainda é o escolar.",
+    presenters: [
+      "Sílvia de Fátima Pilegi Rodrigues (UFR)",
+      "Ilsa do Carmo Vieira Goulart (Universidade Federal de Lavras)",
+      "Emanuela Medeiros (UERN)",
+      "Silvana Ferreira de Souza (Faculdades de Dracena)",
+    ],
+    email: "eixo9cilij@gmail.com",
+  },
+  {
+    eixo: 10,
+    title: "Literatura infantil e as relações com a imagem",
+    resume:
+      "Desde sua origem, a literatura para crianças é atrelada às imagens. As primeiras narrativas destinadas ao público infantil foram recolhidas da tradição oral, adaptadas para o novo público e ilustradas. Apesar de apresentar uma função decorativa, não sendo fundamental para a compreensão da história, a ilustração era concebida como importante para chamar a atenção dos leitores em formação. Entre essas primeiras histórias, encontram-se, no século XVII, as fábulas de La Fontaine e os contos de Charles Perrault. De lá para cá, a ilustração vem ocupando cada vez mais espaço nas páginas dos livros infantis, sendo influenciada pelas demais artes visuais, como a pintura, a fotografia e o cinema. Como destaca Graça Ramos, em A imagem nos livros infantis: caminhos para ler o texto visual (2013, p. 51), “são muitos os teóricos que, ao analisarem a história dos livros infantis, destacam sua inventividade por tornar tão próxima a relação entre as palavras e as imagens.” Atualmente, com os avanços das tecnologias digitais, as obras infantis têm cada vez mais se constituído em objetos multimodais, em que a palavra escrita e a imagem relacionam-se a diversas semioses ou linguagens, como som, fala e imagem em movimento. Este Eixo tem por objetivo discutir a relação entre texto escrito e texto visual na literatura infantil, impressa e/ou digital, considerando suas características multissemióticas. Pretendemos discutir, ainda, a recepção dessas obras pelo público infantil e possibilidades de usos pedagógicos em espaços formais e não formais de educação.",
+    presenters: [
+      "Marta Passos Pinheiro (CEFET-MG)",
+      "Rogério Barbosa da Silva (CEFET-MG)",
+      "Márcia Tavares (UFCG)",
+      "Diana Navas (PUC-SP)",
+    ],
+    email: "eixo10cilij@gmail.com",
+  },
+  {
+    eixo: 11,
+    title: "Literatura infantil e juvenil e outras áreas do conhecimento",
+    resume:
+      "Temas do mundo científico, como aqueles da Astronomia (Sol, Lua, estrelas, corpos celestes etc.), da Climatologia (nuvens, chuva, ventos, fenômenos da natureza etc.), da Geologia (pedras, fósseis, movimentos e processos no interior e na superfície terrestre etc.), da Biologia (animais, evolução, ciclo de vida, organismos vivos etc.) ou da Ecologia (águas, mares, florestas, humanos etc.) são cada vez mais frequentes nas obras de literatura infantil e juvenil disponíveis no mercado brasileiro. Sem se constituírem em cartilhas ou livros de atividades, paradidáticos ou enciclopédicos, estas obras trazem, com frequência, narrativas literárias com personagens ou temas de fundo nos quais estas questões são enfocadas. Assim, é fecunda a discussão sobre como estes temas são abordados em histórias publicadas para crianças, adolescentes e jovens, quer em livros ilustrados, quer em livros apenas de imagens, sem cair no didatismo ou incorporar informações cientificamente equivocadas. Nesse sentido, este Eixo focaliza as discussões dessa interrelação de linguagens e saberes em interdisciplinaridade, aceitando tanto ensaios teóricos sobre a articulação entre qualquer um desses temas e a literatura infantil e juvenil, como análises de obras específicas que os abordem (como, por exemplo: a evolução de um tema em livros de diferentes anos de publicação; a fidedignidade do conteúdo; a forma de apresentação de determinado tema; a comparação entre obras de autores estrangeiros traduzidas e as de autores nacionais; o cotejo entre obras sobre um mesmo tema; a ecocrítica nas elaborações poéticas etc.) e também trabalhos que explorem a leitura de uma ou mais obras sobre tais temáticas pela perspectiva de profissionais da educação, mediadores, contadores de histórias, crianças e/ou adolescentes.",
+    presenters: [
+      "Fabiano Tadeu Grazioli (Univ. Regional Integrada do Alto Uruguai e das Missões)",
+      "Mariana Cortez (UNILA)",
+      "Antônio Cézar Nascimento de Brito (Faculdade Projeção - Brasília)",
+      "Luís Girão (USP)",
+    ],
+    email: "eixo11cilij@gmail.com",
+  },
+];
+
+export default Eixos;
